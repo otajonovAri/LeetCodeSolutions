e2507c98dec396e8fefd3fc5dce980d033a07287
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-09-04 06:50:25 +0500

    Add RemoveDuplicates method and unit tests
    Introduces the `P0026` class with a `RemoveDuplicates` method to remove duplicates from a sorted array. Also adds the `T0026` test class with unit tests using xUnit, covering various scenarios and edge cases to ensure the method's correctness.

---
+++ LeetcodeSolution/0000-1000/P0026.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeSolution._0000_1000;
+
+public class P0026
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0) return 0;
+
+        int x = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[x] = nums[i];
+                x++;
+            }
+        }
+
+        return x;
+    }
+}
---
+++ LeetcodeTest/0000-1000/T0026.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeSolution._0000_1000;
+
+namespace LeetcodeTest._0000_1000;
+
+public class T0026
+{
+    private readonly P0026 _solution = new P0026();
+
+    [Fact]
+    public void RemoveDuplicates_Returns_CaseOne()
+    {
+        int[] nums = { 1, 1, 2 };
+
+        var res = _solution.RemoveDuplicates(nums);
+
+        Assert.Equal(1 , res);
+    }
+
+    [Fact]
+    public void RemoveDuplicates_Returns_CaseTwo()
+    {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+        var res = _solution.RemoveDuplicates(nums);
+
+
+        Assert.Equal(4 , res);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 3, 21 } , 0)]
+    [InlineData(new int[] { 1, 1, 1, 1, 1, 1 } , 5)]
+    [InlineData(new int[] { } , 0)]
+    public void RemoveDuplicates_Returns_MultiplyCases(int[] nums , int exp)
+    {
+        var result = _solution.RemoveDuplicates(nums);
+        Assert.Equal(exp , result);
+    }
+}
