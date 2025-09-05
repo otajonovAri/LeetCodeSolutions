8c39910c32f590a3f511d4876dab94e1ba05fee9
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-09-05 15:38:30 +0500

    Add ContainsDuplicate solution and unit tests
    Introduced a new class `P0217` in the namespace `LeetCodeSolution._0000_1000` that implements the `ContainsDuplicate` method to check for duplicate integers in an array. Added a corresponding test class `T0217` in the namespace `LeetcodeTest._0000_1000`, which includes multiple unit tests, both fact-based and theory-based, to validate the functionality of the `ContainsDuplicate` method.

---
+++ LeetcodeSolution/0000-1000/P0217.cs
@@ -0,0 +1,7 @@
+﻿namespace LeetCodeSolution._0000_1000;
+
+public class P0217
+{
+    public bool ContainsDuplicate(int[] nums)
+        => nums.Distinct().ToArray().Length != nums.Length;
+}
---
+++ LeetcodeTest/0000-1000/T0217.cs
@@ -0,0 +1,46 @@
+﻿using LeetCodeSolution._0000_1000;
+
+namespace LeetcodeTest._0000_1000;
+
+public class T0217
+{
+    private readonly P0217 _solution = new P0217();
+    [Fact]
+    public void ContainsDuplicate_Returns_CaseOne()
+    {
+        int[] nums = { 1, 2, 3, 1 };
+
+        var result = _solution.ContainsDuplicate(nums);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void ContainsDuplicate_Returns_CaseTwo()
+    {
+        int[] nums = { 1, 2, 3, 4 };
+
+        var result = _solution.ContainsDuplicate(nums);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void ContainsDuplicate_Returns_CaseThere()
+    {
+        int[] nums = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+
+        var result = _solution.ContainsDuplicate(nums);
+
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData(new int[] { } , false)]
+    [InlineData(new int[] { 10, 10, 10 } , true)]
+    public void ContainsDuplicate_All_CaseThere(int[] nums , bool exp)
+    {
+        var result = _solution.ContainsDuplicate(nums);
+        Assert.Equal(exp , result);
+    }
+}
